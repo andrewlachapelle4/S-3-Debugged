6d417c23ce9a470c4185f6bf1ab932cac8291c48
andrewlachapelle4 <66648434+andrewlachapelle4@users.noreply.github.com>
2020-12-14 19:05:53 -0800

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using S3_2.Models;
+
+namespace S3_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        ILogger<HomeController> _logger = null;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            this._logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return RedirectToAction("AccelerationProfile");
+        }
+
+    
+        public IActionResult AccelerationProfile()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
